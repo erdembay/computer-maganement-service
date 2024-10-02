8c0cd9701aadfbd41cd89facee51f36ed52c0357
Erdem Bay <erdem.bay@mobilulasim.com>
2024-10-02 14:09:04 +0300

    cihaz bilgileri alındı

--- index.js
+++ index.js
@@ -1,52 +1,77 @@
-const express = require('express');
-const httpStatus = require('http-status');
-const { exec } = require('child_process');
+const express = require("express");
+const httpStatus = require("http-status");
+const { hostname } = require("os");
+const { Netmask } = require("netmask");
+const { get_active_interface } = require("network");
+const { exec } = require("child_process");
 const app = express();
 const PORT = 3000; // HTTP sunucusu bu portta çalışacak
 app.use(express.json());
 // CORS ayarları (güvenlik için)
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
 // Bilgisayarı kapatma isteği
-app.get('/shutdown', (req, res) => {
-  console.log(req);  
-  exec('shutdown /s /t 1', (error, stdout, stderr) => {
+app.get("/shutdown", (req, res) => {
+  console.log(req);
+  exec("shutdown /s /t 1", (error, stdout, stderr) => {
     if (error) {
       console.error(`Kapatma hatası: ${error.message}`);
-      return res.status(httpStatus.INTERNAL_SERVER_ERROR).send(`Hata: ${error.message}`);
+      return res
+        .status(httpStatus.INTERNAL_SERVER_ERROR)
+        .send(`Hata: ${error.message}`);
     }
-    console.log('Bilgisayar kapatılıyor...');
-    res.status(httpStatus.OK).send({message:'Bilgisayar kapatılıyor...'});
+    console.log("Bilgisayar kapatılıyor...");
+    res.status(httpStatus.OK).send({ message: "Bilgisayar kapatılıyor..." });
   });
 });
 
 // Bilgisayarı yeniden başlatma isteği
-app.post('/restart', (req, res) => {
-  exec('shutdown /r /t 1', (error, stdout, stderr) => {
+app.post("/restart", (req, res) => {
+  exec("shutdown /r /t 1", (error, stdout, stderr) => {
     if (error) {
       console.error(`Yeniden başlatma hatası: ${error.message}`);
       return res.status(500).send(`Hata: ${error.message}`);
     }
-    console.log('Bilgisayar yeniden başlatılıyor...');
-    res.send('Bilgisayar yeniden başlatılıyor...');
+    console.log("Bilgisayar yeniden başlatılıyor...");
+    res.send("Bilgisayar yeniden başlatılıyor...");
   });
 });
+//NODEJS Bilgisayarın adını alma
+function getNetworkInterfaces() {
+  return new Promise((resolve, reject) => {
+    get_active_interface((err, activeInterface) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(activeInterface);
+      }
+    });
+  });
+}
 // Bilgisayarı yeniden başlatma isteği
-app.get('/info', (req, res) => {
+app.get("/info", async (req, res) => {
+  const computerName = await hostname();
+  const networkInterfaces = await getNetworkInterfaces();
+  const block = await new Netmask(
+    `${networkInterfaces.ip_address}/${networkInterfaces.netmask}`
+  );
   res.status(httpStatus.OK).send({
-    name: "anan",
-    mac: "mac",
-    ip: "192.168.1.163",
-    ping: "13132123",
+    name: computerName,
+    mac: networkInterfaces?.mac_address,
+    ip: networkInterfaces?.ip_address,
+    broadcast: block?.broadcast,
     port: "9",
   });
 });
-app.get('/', (req, res) => {
-  res.status(200).send({status:true, message:"index"})
+app.get("/", (req, res) => {
+  res.status(200).send({ status: true, message: "index" });
 });
 // Sunucuyu başlat
 app.listen(PORT, () => {
